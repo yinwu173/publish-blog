1a16a1133292f057b6d2fc3363a71f8a99755375
yinwu173 <yin.wu173@gmail.com>
2024-10-08 20:29:54 -0700

    edited index.js in the models folder: added relationships for User, Post, and Comment

--- models/index.js
+++ models/index.js
@@ -1,7 +1,12 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
     foreignKey: 'user_id'
 });
 
@@ -9,6 +14,17 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+Post.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
 
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };
