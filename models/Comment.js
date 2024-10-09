d08bd90b19e26b77f842d6d8c2d945e2e5bc3a58
yinwu173 <yin.wu173@gmail.com>
2024-10-08 20:04:55 -0700

    added Comment class model then exported - since the code of similar to my Project #2 assignment, I utilized most of the code from that project

---
+++ models/Comment.js
@@ -0,0 +1,46 @@
+// Obtained the following lines of code from my work done in Project #2 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 100]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+
+module.exports = Comment;
